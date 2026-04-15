b26e780a18fd2781a2cd2d7ff2a848c467857405
HeitorDalla <heitorvillavilla@gmail.com>
2026-04-15 19:18:46 -0300

    feat: adicionar entidade Produto com atributos necessarios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,11 +36,12 @@ android {
 }
 
 dependencies {
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation(libs.room.common.jvm)
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
 }
